1cf0bbad5adbc2e3c9f09ea381e1a7906209bec1
SALIMA <salima_handouch@hotmail.com>
2015-10-11 17:15:18 +0200

    mise a jour libs

--- src/test/groovy/fr/uvsq/doosa/gbintool1/MainTest.groovy
+++ src/test/groovy/fr/uvsq/doosa/gbintool1/MainTest.groovy
@@ -2,21 +2,12 @@ package fr.uvsq.doosa.gbintool1
 
 import org.junit.Test
 
+
+
 class MainTest {
-    @Test
+   @Test
    void shouldReturnHello() {
-       Main m = new Main()
-    assert 'Hello' == m.returnHello()
-    }
-
-    @Test
-        void shouldHaveTheCorrectNumberOfObjects() {
-        assert starTable.rowCount == rowCount
-        }
-
-    @Test
-    void shouldHaveTheCorrectParameters() {
-    def parameterValues = starTable.parameters.collect { it.getValue() }
-    assert parameters == parameterValues.join(", ")
-    }
+Main m = new Main()
+assert 'Hello' == m.returnHello()
+}
 }
